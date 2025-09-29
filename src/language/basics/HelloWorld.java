17cfe884d55a0f541efa2f43aabf53a576426333
Shikha Sharma <sssshiks@gmail.com>
2025-09-29 18:41:25 +0530

    initial commit

---
+++ src/language/basics/HelloWorld.java
@@ -0,0 +1,21 @@
+package language.basics;
+
+public class HelloWorld {
+	
+	// This is a main method, And, Always JVM begin the execution from main method
+	// Comments in Java are mainly three different categories.
+	// 1. Single Line Comments ==> //
+	// 2. multi-line Comments ==> /* .... */ 
+	// 3. Documentation Comments ==> /** ... */
+	
+	//E
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	
+
+	}
+
+}
